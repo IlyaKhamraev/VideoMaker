269d30687fb3d2467a376e4d96d3b6c8ec3af6cc
Ilya Khamraev <hamraev9517@gmail.com>
2023-05-22 22:13:57 +0500

    fix(films): added iframe

--- src/components/FilmsList/Film/index.tsx
+++ src/components/FilmsList/Film/index.tsx
@@ -3,5 +3,18 @@ import { useParams } from "react-router-dom";
 export const Film = () => {
   const { id } = useParams();
 
-  return <div>FILM {id}</div>;
+  return (
+    <div>
+      FILM {id}
+      <div>
+        <iframe
+          title="test"
+          scrolling="no"
+          frameBorder="0"
+          allowFullScreen
+          src="https://www.youtube.com/embed/xB4nyimch8E"
+        />
+      </div>
+    </div>
+  );
 };
