935f8f717a523102e9bc3c020eced1e0475a2884
Ilya Khamraev <hamraev9517@gmail.com>
2023-05-24 22:48:21 +0500

    fix(dashboard): dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,3 +1,20 @@
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>preview (фото заставки)</td>
+          <td>организация</td>
+          <td>название ролика</td>
+          <td>мероприятие</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
