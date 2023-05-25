069d5d94ea556be8a29f63dd9a83506c031a9490
Ilya Khamraev <hamraev9517@gmail.com>
2023-05-25 23:15:02 +0500

    fix(dashboard): git push

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -9,10 +9,10 @@ export const Dashboard = () => {
           <td>мероприятие</td>
         </tr>
         <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td>4</td>
+          <td>тест</td>
+          <td>тест</td>
+          <td>тест</td>
+          <td>тест</td>
         </tr>
       </table>
     </div>
