6e692bafa5a9fb77174ba999dbc101a0d36a1f2d
Ilya Khamraev <hamraev9517@gmail.com>
2023-09-18 20:26:59 +0500

    feat(film): added play icon

--- src/components/FilmsList/FilmItem/index.tsx
+++ src/components/FilmsList/FilmItem/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
+import { GiPlayButton } from "react-icons/gi";
 
 import styles from "components/FilmsList/FilmItem/styles.module.css";
 
@@ -22,6 +23,12 @@ export const FilmItem: FC<Props> = ({
     <Link className={styles.linkWrapper} to={`film/${id}`}>
       <div className={styles.filmImg}>
         <img className={styles.img} src={promoImg} alt="promo" />
+
+        <Link className={styles.playLink} to={`film/${id}`}>
+          <span className={styles.play}>
+            <GiPlayButton />
+          </span>
+        </Link>
       </div>
       <div className={styles.content}>
         <p className={styles.client}>{client}</p>
